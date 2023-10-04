632678036cb9765bd5252e6a04180a58a2877db6
Rene Dario Tapia Alcaraz <88797997+ReneDTapia@users.noreply.github.com>
2023-10-03 22:37:08 -0600

    Initial Commit

---
+++ Phil/PhilApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhilApp.swift
+//  Phil
+//
+//  Created by Rene  on 03/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PhilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
