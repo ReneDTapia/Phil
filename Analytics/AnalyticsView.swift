a7ac4db4e10601f3f59b1e01de7e23d2de52b987
LG <92387298+LGnan@users.noreply.github.com>
2023-11-09 22:08:20 -0600

    Avance front bueno xd, ya casi va (sin miedo al babugazo)

--- Analytics/AnalyticsView.swift
+++ Analytics/AnalyticsView.swift
@@ -15,7 +15,6 @@ struct ContentView: View{
         GeometryReader{
             
             geometry in
-            
             ZStack(alignment: .leading) {
                 Color.black
                     .ignoresSafeArea(.all)
@@ -40,12 +39,13 @@ struct ContentView: View{
                     Text("Metas Diarias")
                         .font(.largeTitle)
                         .bold()
-                        .padding(EdgeInsets(top: 20, leading: 20, bottom: 0, trailing: 10))
+                        .padding(EdgeInsets(top: 20, leading: 20, bottom: 8, trailing: 10))
                         .foregroundColor(.white)
                     Spacer()
                     
                     // Aqui
                     BarChart()
+                    
                     
                 }.padding()
                 
@@ -82,33 +82,130 @@ struct ContentView: View{
 }
 
 struct BarChart: View {
-    // atributo que lea el rect
-    var body : some View{
-        ZStack{
-            //Rectangle 2
-            RoundedRectangle(cornerRadius: 17)
-                .fill(Color(hex: "FFFFFF"))
-            .frame(width: 345, height: 232)
-            .shadow(color: Color(hex: "B9B6B6"), radius:4, x:0, y:0)
-            VStack{
-                //Your feelings last days
-                Text("Your feelings last days").font(.custom("Inter Semi Bold", size: 20)).tracking(-0.41).multilineTextAlignment(.center)
-                    .bold()
-                    .padding(.bottom, 180)
-            }
-            HStack{
-                ForEach(0..<5) { _ in
-                    Rectangle()
-                        .fill(Color(hex: "6B6EAB"))
-                        .frame(width: 20, height: 80) // Ajusta la altura según sea necesario
-                        .cornerRadius(10)
+    // Arreglo de alturas para cada barra de la gráfica
+    let values: [CGFloat] = [51, 20, 30, 20, 80]
+
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                RoundedRectangle(cornerRadius: 17)
+                    .fill(Color.white)
+                    .frame(width: geometry.size.width, height: geometry.size.height/2.5)
+                    .shadow(color: Color(hex: "B9B6B6"), radius:2, x:0, y:0)
+                
+                ZStack{
+                    HStack{
+                        VStack(alignment: .trailing){
+                            ForEach(0..<6) { index in
+                                Text("\(100 - index * 20)%")
+                                    .font(.custom("Inter Semi Bold", size: 15))
+                                    .foregroundColor(Color.black)
+                            }.padding(.bottom, 1)
+                        }
+                        VStack(spacing: 22){
+                            ForEach(0..<6) { index in
+                                Divider()
+                                    .background(Color.gray)
+                                    .opacity(0.5)
+                            }.padding(.bottom, 1)
+                        }
+                    }
+                    .frame(width: geometry.size.width/1.1, height: geometry.size.height/2.5)
+                    VStack {
+                        Text("Your feelings last days")
+                            .font(.custom("Inter Semi Bold", size: 24))
+                            .tracking(-0.41)
+                            .multilineTextAlignment(.center)
+                            .padding(.bottom, 188)
+                    }
+                    
+                    HStack(spacing: 22) {
+                        Spacer()
                         
+                        ForEach(0..<values.count, id:\.self) { index in
+                            VStack {
+                                Spacer()
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color(hex: "6B6EAB"))
+                                    .frame(width: 20, height: values[index])
+                                    .clipShape(Rectangle().offset(y: -10))
+                                    
+                            }
+                            .padding(.leading, 15)
+                            .padding(.trailing, 3)
+                        }
+                    }.padding(.bottom, 218)
+                        .padding(.trailing, 25)
                 }
             }
         }
-        
     }
 }
+
+
+
+
+
+
+
+/*
+ struct BarChart: View {
+ var body: some View {
+ GeometryReader { geometry in
+ ZStack {
+ RoundedRectangle(cornerRadius: 17)
+ .fill(Color(hex: "FFFFFF"))
+ .frame(width: geometry.size.width, height: geometry.size.height/2.5)
+ .shadow(color: Color(hex: "B9B6B6"), radius:2, x:0, y:0)
+ 
+ ZStack{
+ 
+ // Líneas de fondo
+ VStack{
+ ForEach(0..<6) { index in
+ HStack{
+ VStack{
+ Text("\(100 - index * 20)%") // Agrega los porcentajes
+ .font(.custom("Inter Semi Bold", size: 10))
+ .foregroundColor(Color(hex:"000000"))
+ 
+ }.padding(.trailing, 8)
+ VStack{
+ Divider()
+ .background(Color.gray)
+ .opacity(0.5)
+ }
+ }
+ }
+ }
+ .frame(width: geometry.size.width/1.1, height: geometry.size.height/2.5) // Ajusta el ancho de las líneas de fondo
+ .padding(.horizontal)
+ 
+ VStack {
+ Text("Your feelings last days")
+ .font(.custom("Inter Semi Bold", size: 24))
+ .tracking(-0.41)
+ .multilineTextAlignment(.center)
+ .padding(.bottom, 180)
+ }
+ 
+ HStack {
+ ForEach(0..<5) { _ in
+ Rectangle()
+ .fill(Color(hex: "6B6EAB"))
+ .frame(width: 20, height: 51)
+ .cornerRadius(10)
+ .padding(10)
+ }
+ }
+ }
+ }
+ }
+ }
+ }
+ */
+
+
 
 
 #Preview{
